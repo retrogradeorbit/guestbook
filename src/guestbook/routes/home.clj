853aaf31106973e5c90897c26ba2e0e21bddf6a2
Crispin Wellington <retrogradeorbit@gmail.com>
2015-09-13 10:30:50 +0800

    views for guestbook app

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -1,17 +1,42 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :refer [ok]]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
 
-(defn home-page []
-  (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+            [guestbook.db.core :as db]
+            [bouncer.core :as bouncer]
+            [bouncer.validators :as validators]
+            [ring.util.response :refer [redirect]]
+
+            ))
 
 (defn about-page []
   (layout/render "about.html"))
 
-(defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+(defn validate-message [params]
+  (first
+    (bouncer/validate
+      params
+      :name validators/required
+      :message [validators/required [validators/min-count 10]])))
 
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (redirect "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message!
+       (assoc params :timestamp (java.util.Date.)))
+      (redirect "/"))))
+
+(defn home-page [{:keys [flash]}]
+  (layout/render
+   "home.html"
+   (merge {:messages (db/get-messages)}
+          (select-keys flash [:name :message :errors]))))
+
+(defroutes home-routes
+  (GET "/" request (home-page request))
+  (POST "/" request (save-message! request))
+  (GET "/about" [] (about-page)))
